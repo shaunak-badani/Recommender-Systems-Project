bb7579801ce6a58d1dd63b003193536f7bf36c62
iaravagni <iara.ravagni@duke.edu>
2025-04-13 17:52:37 -0400

    inference file fixed

--- backend/src/methods/deep_learning/inference.py
+++ backend/src/methods/deep_learning/inference.py
@@ -6,7 +6,7 @@ import numpy as np
 import joblib
 
 # Add the project root directory to Python path
-project_root = os.path.abspath(os.path.join(os.path.dirname(_file_), '../../../..'))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..'))
 sys.path.append(project_root)
 from scripts.model_training import DeepRecommender, business_data_preprocessing, user_data_preprocessing
 
